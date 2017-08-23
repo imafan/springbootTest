0b7ee23df34a2814770d8b3efceb7c5b15c527c8
imafan <358462953@qq.com>
2017-08-23 11:39:20 +0800

    controller单元测试

--- src/test/java/com/example/demo/HelloControllerTests.java
+++ src/test/java/com/example/demo/HelloControllerTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,23 +8,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 /**
  * Created by imafan on 2017-08-22.
  */
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class HelloControllerTests {
 
-    @Test
-    public void contextLoads() {
-        System.out.println(new HelloController().say());
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
     }
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.equalTo("say 111")));
+    }
+
+//    @Test
+//    public void contextLoads() {
+//        System.out.println(new HelloController().say());
+//    }
 }
