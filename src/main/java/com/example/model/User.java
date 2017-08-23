6db6129ff4bc0ebad85258aba18b2ebabf1cc5bd
imafan <358462953@qq.com>
2017-08-23 15:07:08 +0800

    RESTful API与单元测试

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,34 @@
+package com.example.model;
+
+/**
+ * Created by imafan on 2017-08-23.
+ */
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/test/java/com/example/demo/UserControllerTests.java
@@ -0,0 +1,89 @@
+package com.example.demo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by imafan on 2017-08-23.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+@WebAppConfiguration
+public class UserControllerTests {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+
+//        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testUserController() throws Exception {
+        // 测试UserController
+        RequestBuilder request = null;
+
+        // 1、get查一下user列表，应该为空
+        request = get("/user/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+        // 2、post提交一个user
+        request = post("/user/")
+                .param("id", "1")
+                .param("name", "测试大师")
+                .param("age", "20");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        // 3、get获取user列表，应该有刚才插入的数据
+        request = get("/user/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")));
+
+        // 4、put修改id为1的user
+        request = put("/user/1")
+                .param("name", "测试终极大师")
+                .param("age", "30");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        // 5、get一个id为1的user
+        request = get("/user/1");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")));
+
+        // 6、del删除id为1的user
+        request = delete("/user/1");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        // 7、get查一下user列表，应该为空
+        request = get("/user/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+    }
+}
