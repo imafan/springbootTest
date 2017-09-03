7a749bff248200377e66269f233441f1c7345a13
imafan <358462953@qq.com>
2017-09-03 21:48:19 +0800

    增加mybatis（xml）

---
+++ src/main/java/com/example/enums/UserSexEnum.java
@@ -0,0 +1,8 @@
+package com.example.enums;
+
+/**
+ * Created by imafan on 2017/9/3 0003.
+ */
+public enum UserSexEnum {
+    MAN, WOMAN
+}
--- src/main/java/com/example/mapper/UserMapper.java
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -1,25 +1,24 @@
 package com.example.mapper;
 
-import com.example.model.User;
-import org.apache.ibatis.annotations.Insert;
+import com.example.entity.User;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 /**
  * Created by imafan on 2017-08-24.
  */
-
 @Mapper
 public interface UserMapper {
 
-    @Select("SELECT * FROM USER WHERE NAME = #{name}")
-    User findByName(@Param("name") String name);
+    List<User> getAll();
 
-    List<User> findAll();
+    User getOne(Long id);
 
-    @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name}, #{age})")
-    int insert(@Param("name") String name, @Param("age") Integer age);
+    void insert(User user);
+
+    void update(User user);
+
+    void delete(Long id);
+
 }
