dd042ee1db0da4da6e97c73c1066d738a78e1af7
imafan <358462953@qq.com>
2017-09-07 16:00:46 +0800

    异步调用

---
+++ src/test/java/com/example/demo/AsyncTest.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import com.example.async.AsyncTask;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.Future;
+
+/**
+ * Created by imafan on 2017-09-07.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AsyncTest {
+
+    @Autowired
+    private AsyncTask task;
+
+
+    @Test
+    public void test() throws Exception {
+        long start = System.currentTimeMillis();
+        Future<String> task1 = task.doTaskOne();
+        Future<String> task2 = task.doTaskTwo();
+        Future<String> task3 = task.doTaskThree();
+        while(true) {
+            if(task1.isDone() && task2.isDone() && task3.isDone()) {
+                // 三个任务都调用完成，退出循环等待
+                break;
+            }
+            Thread.sleep(1000);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
+    }
+}
