15d4e70c1a1fe359b5652efed98221f8d2a5bffa
imafan <358462953@qq.com>
2017-09-08 16:45:40 +0800

    整合 Redis 实现缓存操作

--- src/main/java/com/example/redis/RedisConfig.java
+++ src/main/java/com/example/redis/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.example.redis;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -10,13 +9,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 /**
  * Created by imafan on 2017-09-07.
  */
-@Configuration
+//@Configuration
 public class RedisConfig {
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         return new JedisConnectionFactory();
     }
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
---
+++ src/main/java/com/example/service/CityService.java
@@ -0,0 +1,41 @@
+package com.example.service;
+
+import com.example.model.City;
+
+/**
+ * Created by imafan on 2017-09-08.
+ */
+public interface CityService {
+
+    /**
+     * 根据城市 ID,查询城市信息
+     *
+     * @param id
+     * @return
+     */
+    City findCityById(Long id);
+
+    /**
+     * 新增城市信息
+     *
+     * @param city
+     * @return
+     */
+    Long saveCity(City city);
+
+    /**
+     * 更新城市信息
+     *
+     * @param city
+     * @return
+     */
+    Long updateCity(City city);
+
+    /**
+     * 根据城市 ID,删除城市信息
+     *
+     * @param id
+     * @return
+     */
+    Long deleteCity(Long id);
+}
